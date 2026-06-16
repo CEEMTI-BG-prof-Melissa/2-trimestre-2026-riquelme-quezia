0f5a0e3bed3f7f1578d52561980f3807d9b1852d
riquelmekboasquives-art <riquelme.kboasquives@aluno.edu.es.gov.br>
2026-06-16 11:59:04 -0300

    Create index.php

---
+++ Aula-01PHP/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Aula PHP</title>
+    </head>
+    <body>
+
+        <?php
+            echo "<h1> olá!<br> mundo! <h1>";       
+        ?>
+
+
+
+    </body>
+
+</html>
