a15064d5ac0798afe966b6f0e1bc6c4049cedd2b
riquelmekboasquives-art <riquelme.kboasquives@aluno.edu.es.gov.br>
2026-06-24 11:56:41 -0300

    Create index.php

---
+++ aula02/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html lang "pt-br">
+
+    <head> 
+        <meta charset="UTF-8">
+        <title> Aula 02 HTML </title>
+    </head>
+    <body>
+
+            <?php
+                $nome1= "Riquelme";
+                $nome2= "Quezia";
+                $idade1= "16";
+                $idade2= "16";
+
+                echo "meu nome é " .$nome2. " eu tenho " .$idade2 ."anos, minha dupla se chama " .$nome1 ." e sua idade é ". $idade1;
+            ?>
+
+
+
+
+
+
+
+    </body>
+    </html>
